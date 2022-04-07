33933516ee9d252fafb4e0de932205070a919988
Thomas <ttomtsis@protonmail.com>
2022-04-07 18:23:15 +0300

    Node Class fields set

--- node.py
+++ node.py
@@ -1,5 +1,13 @@
 class MyNode:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-        print("node created")
+
+    def __init__(self, n, u, d, l, r, wu, wd, wl, wr):
+        self.name = str(n)
+        self.up = bool(u)
+        self.down = bool(d)
+        self.left = bool(l)
+        self.right = bool(r)
+        self.weight_up = bool(wu)
+        self.weight_down = bool(wd)
+        self.weight_left - bool(wl)
+        self.weight_right = bool(wr)
+        print("Node " + n + " Created")
