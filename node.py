be80be92ae23133fce5f0c3479840ea57532fb47
Thomas <ttomtsis@protonmail.com>
2022-04-08 16:34:24 +0300

    Added print Fixed minor issue

--- node.py
+++ node.py
@@ -10,3 +10,11 @@ class MyNode:
         self.weight_down = 0
         self.weight_left = 0
         self.weight_right = 0
+
+    def print(self):
+        print("----- " + self.name + " -----")
+        print("Up: " + str(self.up) + " - " + str(self.weight_up))
+        print("Down: " + str(self.down) + " - " + str(self.weight_down))
+        print("Left: " + str(self.left) + " - " + str(self.weight_left))
+        print("Right: " + str(self.right) + " - " + str(self.weight_right))
+
