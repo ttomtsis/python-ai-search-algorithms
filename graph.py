a9b63544909c951b64a4fd76bb2e23337b068341
Thomas <ttomtsis@protonmail.com>
2022-04-17 00:37:55 +0300

    Added comments

--- graph.py
+++ graph.py
@@ -1,23 +1,35 @@
+"""
+This file contains the graph class,
+used to construct a graph of size n x n
+alongside with the appropriate methods to handle it.
+The graph is a simple "2-D list" containing the nodes
+"""
+
 import random
 import node
 
 
 class Graph:
 
-    def __init__(self, nodes, percent, max):
+    # Constructor takes as parameter dimensions, percent of
+    # edges to be removed and max edge cost
+    def __init__(self, n_dimension, percent, max):
         self.max_weight = max
-        self.n = nodes
+        self.n = n_dimension
         self.p = percent
         self.start_state = None
         self.end_state_1 = None
         self.end_state_2 = None
-        self.nodesList = []
+        self.nodesList = []  # Nodes are stored here, this is the "graph"
+
+        # Create the graph
         self.create_graph()
         self.remove_edges()
         self.set_states()
         self.create_heuristics()
         self.print_graph()
 
+    # Prints the graph
     def print_graph(self):
 
         for x in range(0, self.n):
@@ -26,16 +38,19 @@ class Graph:
                 ni.print()
         print("\n")
 
+    # Support function, used by create_Graph.
+    # Returns a random number from 0 to maximum cost of the edges
     def rand_weight(self):
         return int(random.randrange(1, self.max_weight))
 
+    # Creates the 2-D list that contains the nodes
     def create_graph(self):
         print("Creating Graph")
         print("Size will be: " + str(self.n) + " X " + str(self.n))
         counter = 1
 
-        # Dhmiourgia antikeimenon
-        # topothetisi se "pinaka"
+        # Create node objects
+        # place them in lists
         for x in range(0, self.n):
             cols = []
             for y in range(0, self.n):
@@ -45,32 +60,30 @@ class Graph:
                 cols.append(ni)
                 counter += 1
             self.nodesList.append(cols)
-        # ni: node.MyNode = nodes[1][0]
-        # print(ni.name)
 
-        # Set connections
+        # Create Edges
         for x in range(0, self.n):
 
             for y in range(0, self.n):
 
                 temp: node.MyNode = self.nodesList[x][y]
-                # print("x: " + str(x) + " y: " + str(y))
-                # print("Setting : " + temp.name)
 
-                # Does not go up
+                # If x is 0, a.k.a the first line in a 2-D array,
+                # then the nodes cannot have a neighbour upwards
                 if x == 0:
 
-                    # Does not go left AND does not go up
+                    # If y is 0, a.k.a. the first column in a 2-D array,
+                    # then the nodes cannot have a neighbour on the left
+                    # so to sum it up this is the case where the nodes
+                    # only have a neighbour down and on the right
                     if y == 0:
-                        # print("x==0 Does not go left AND does not go up")
                         temp.right = self.nodesList[x][y + 1]
                         temp.weight_right = self.rand_weight()
                         temp.down = self.nodesList[x + 1][y]
                         temp.weight_down = self.rand_weight()
 
-                    # Does not go right AND does not go up
+                    # In similar fashion, cannot have edges upwards and to the right
                     elif y == self.n - 1:
-                        # print("x==0 Does not go right AND does not go up")
                         temp.down = self.nodesList[x + 1][y]
                         temp.weight_down = self.rand_weight()
                         temp.left = self.nodesList[x][y - 1]
@@ -86,9 +99,12 @@ class Graph:
                         temp.left = self.nodesList[x][y - 1]
                         temp.weight_left = self.rand_weight()
 
-                # Does not go down
+                # If x = n-1, a.k.a the last line of a 2-D array,
+                # then the edges cannot be downwards
                 elif x == self.n - 1:
 
+                    # In similar fashion we check the columns
+                    # and adjust the edges accordingly
                     if y == self.n - 1:
                         temp.left = self.nodesList[x][y - 1]
                         temp.weight_left = self.rand_weight()
@@ -107,10 +123,10 @@ class Graph:
                         temp.up = self.nodesList[x - 1][y]
                         temp.weight_up = self.rand_weight()
 
-                # General case
+                # General case, nodes can have edges on any direction.
+                # however, column conditions still apply, as shown below
                 else:
                     if y == self.n - 1:
-                        # print("General - Does not go right AND does not go up")
                         temp.down = self.nodesList[x + 1][y]
                         temp.weight_down = self.rand_weight()
                         temp.up = self.nodesList[x - 1][y]
@@ -125,7 +141,6 @@ class Graph:
                         temp.right = self.nodesList[x][y + 1]
                         temp.weight_right = self.rand_weight()
                     else:
-                        # print("General - General case")
                         temp.right = self.nodesList[x][y + 1]
                         temp.weight_right = self.rand_weight()
                         temp.down = self.nodesList[x + 1][y]
@@ -137,18 +152,32 @@ class Graph:
 
         print("Graph Created")
 
+    # Removes a percentage of edges from the graph
+    # edges to be removed from each node, as well as
+    # how many edges per node are to be removed is random.
+    # See remove_Edge() in node.py for more information,
+    # This function simply chooses a random node, the rest
+    # is handled by the node class's inherent method
     def remove_edges(self):
 
         print("Removing Edges")
-        total = 2 * 2 * self.n * (self.n - 1)  # Afou einai amfidromo tote prepei na einai *2 o typos tou fyladiou
+        total = 2 * 2 * self.n * (self.n - 1)  # Total edges of the graph
         print("Total Edges : " + str(total))
-        to_remove = int((self.p * total) / 100)
+        to_remove = int((self.p * total) / 100)  # Final number of removed edges
         print("Will remove : " + str(to_remove))
 
+        # Loop to_remove times, each time a random edge, from a random node
+        # is removed.
         i = 0
         while i < to_remove:
+
+            # Choose a random node
             random_node = random.randrange(1, (self.n * self.n) + 1)
-            # print("Random Node: " + str(random_node))
+
+            # Find node inside graph, remove edges then continue
+            # to next node. found boolean and if conditions below
+            # are used strictly to improve performance and do not
+            # affect the logic described above
             found = False
             for x in range(0, self.n):
                 for y in range(0, self.n):
@@ -158,13 +187,10 @@ class Graph:
                     if ni.name == str(random_node):
                         result = int(ni.remove_edge())
                         if result == 1:
-                            # print("Edge removed from " + ni.name)
-                            # ni.print()
                             i += 1
                             found = True
                             break
                 if found:
-                    # print("Breaking, since found")
                     break
         print("Edges removed")
 
@@ -200,6 +226,9 @@ class Graph:
         print("States Set")
         print("---------------")
 
+    # Supporting method, used by create_heuristics() below
+    # calculates manhattan distance, based on the location
+    # of 2 nodes on the graph
     def calc_distance(self, x1, x2, y1, y2):
         x = x1 - x2
         if x < 0:
@@ -209,6 +238,10 @@ class Graph:
             y = y * (-1)
         return x + y
 
+    # Creates the Heuristics used by A* and greedy BFS algorithms
+    # Chooses the SMALLEST manhattan distance between the 2 end states
+    # then sets the A* cost as equal to the manhattan distance
+    # ( A* cost is calculated during expansion either way )
     def create_heuristics(self):
         print("Creating Heuristic Table")
 
@@ -223,6 +256,10 @@ class Graph:
                     ni.manhattan = dist_2
                 ni.a_star_cost = ni.manhattan
 
+    # Resets the nodes of the graph
+    # so that a new search algorithm can be applied.
+    # this happens since during search numerous class fields
+    # are altered and cannot be re-used for another search
     def reset(self):
 
         for x in range(0, self.n):
